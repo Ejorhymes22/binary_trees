28aa2830455360f8ec632ee0a5a506c57b148711
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-11-15 04:50:31 -0800

    insert

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,6 +20,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
+	new->right = NULL;
+	new->left = NULL;
 	new->n = value;
 	new->parent = parent;
 
