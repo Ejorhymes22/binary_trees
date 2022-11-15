063e58ad16902cddd61c6d08f8b80d3d2929cd73
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-11-15 07:54:31 -0800

    * removed

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,11 +11,9 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree)
-		return;
 	if (tree)
 	{
-		(*func)(tree->n);
+		(func)(tree->n);
 		binary_tree_preorder(tree->left, &(*func));
 		binary_tree_preorder(tree->right, &(*func));
 	}
