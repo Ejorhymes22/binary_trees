9ebe49b7994d3dc2ea6d4285ed15844f1af8f8a8
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-11-15 07:41:59 -0800

    betty fixes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-/*
+
+/**
  * binary_tree_node - creates abinary tree node
  * @parent: the parent node
  * @value: its value
  *
  * Return: pointer to the new node
  */
+
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,7 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node || !(node->parent))
+	if (!node)
 		return (0);
 	if (!(node->right) && !(node->left))
 		return (1);
