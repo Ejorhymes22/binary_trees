0c625ba56bd2c72d2a232cef80c76f8db86b4313
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-11-15 08:02:52 -0800

    if cond

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -14,7 +14,9 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree)
 	{
 		(func)(tree->n);
-		binary_tree_preorder(tree->left, (func));
-		binary_tree_preorder(tree->right, (func));
+		if (tree->left)
+			binary_tree_preorder(tree->left, (func));
+		if (tree->right)
+			binary_tree_preorder(tree->right, (func));
 	}
 }
