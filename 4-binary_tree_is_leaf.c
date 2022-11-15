81a97c57f13bb677f3adefde6f104e71ec563f2d
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-11-15 07:39:34 -0800

    !node

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,11 +5,13 @@
  * binary_tree_is_leaf - checksifnodeisaleaf
  * @node: the node
  *
- * Returns 1 or 0
+ * Return: 1 or 0
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (!node || !(node->parent))
+		return (0);
 	if (!(node->right) && !(node->left))
 		return (1);
 	return (0);
