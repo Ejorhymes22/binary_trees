86fb71e651bf74a5b60974e9468c4d0756331946
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-11-17 03:19:55 -0800

    betty ish

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -2,102 +2,105 @@
 #include "binary_trees.h"
 
 /**
- * rec_binary_tree_levelorder - recursively goes through tree levelorder
- * @tree: the pointer to root
- * @func: fucntion to perform
- * @h: the level
+ * maxValue - of the tree
+ * @node: the node
  *
- * Return: void
+ * Return: max value
  */
-/*
-int rec_binary_tree_is_bst(const binary_tree_t *node, const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (1);
-	if (tree && node != tree)
-	{
-		if (node->n < tree->n)
-			return (0);
-	}
-	else if (node != tree && node->n > tree->n)
-		return (0);
-
-	return (rec_binary_tree_is_bst(node, tree->left) && rec_binary_tree_is_bst(node, tree->right));
-}
-*/
 
 
 int maxValue(const binary_tree_t *node)
 {
-    if (node == NULL) {
-        return 0;
-    }
-
-    int leftMax = maxValue(node->left);
-    int rightMax = maxValue(node->right);
-
-    int value = 0;
-    if (leftMax > rightMax) {
-        value = leftMax;
-    }
-    else {
-        value = rightMax;
-    }
-
-    if (value > node->n) {
-        value = node->n;
-    }
-
-    return value;
+if (node == NULL)
+{
+return (0);
 }
+
+int leftMax = maxValue(node->left);
+int rightMax = maxValue(node->right);
+
+int value = 0;
+if (leftMax > rightMax)
+{
+value = leftMax;
+}
+else
+{
+value = rightMax;
+}
+
+if (value > node->n)
+{
+value = node->n;
+}
+
+return (value);
+}
+
+/**
+ * minValue - of the tree
+ * @node: the node
+ *
+ * Return: max value
+ */
 
 int minValue(const binary_tree_t *node)
 {
-    if (node == NULL) {
-        return 1000000000;
-    }
-
-    int leftMax = minValue(node->left);
-    int rightMax = minValue(node->right);
-
-    int value = 0;
-    if (leftMax < rightMax) {
-        value = leftMax;
-    }
-    else {
-        value = rightMax;
-    }
-
-    if (value < node->n) {
-        value = node->n;
-    }
-
-    return value;
+if (node == NULL)
+{
+return (0);
 }
 
-/* Returns true if a binary tree is a binary search tree */
-int rec_binary_tree_is_bst(const binary_tree_t *node, const binary_tree_t *tree)
+int leftMax = minValue(node->left);
+int rightMax = minValue(node->right);
+
+int value = 0;
+if (leftMax < rightMax)
 {
-    if (tree == NULL)
-        return 1;
+value = leftMax;
+}
+else
+{
+value = rightMax;
+}
 
-    /* false if the max of the left is > than us */
-    if (tree->left != NULL
-        && maxValue(tree->left) > node->n)
-        return 0;
+if (value < node->n)
+{
+value = node->n;
+}
 
-    /* false if the min of the right is <= than us */
-    if (tree->right != NULL
-        && minValue(tree->right) < node->n)
-        return 0;
+return (value);
+}
 
-    /* false if, recursively, the left or right is not a BST
-     */
-    if (rec_binary_tree_is_bst(node, tree->left) && rec_binary_tree_is_bst(node, tree->right))
-        return 0;
+/**
+ * rec_binary_tree_is_bst - rec checks for bst
+ * @node: the main root
+ * @tree: the tree
+ *
+ * Return: 0 or 1
+ */
 
-    /* passing all that, it's a BST */
-    return 1;
+int rec_binary_tree_is_bst(const binary_tree_t *node,
+		const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (1);
+
+/* false if the max of the left is > than us */
+if (tree->left != NULL && maxValue(tree->left) > node->n)
+return (0);
+
+/* false if the min of the right is <= than us */
+if (tree->right != NULL && minValue(tree->right) < node->n)
+return (0);
+
+/* false if, recursively, the left or right is not a BST*/
+if (rec_binary_tree_is_bst(node, tree->left)
+		&& rec_binary_tree_is_bst(node, tree->right))
+return (0);
+
+/* passing all that, it's a BST */
+return (1);
 }
 
 
@@ -117,8 +120,5 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	if (node == NULL)
 		return (0);
 	l = rec_binary_tree_is_bst(node, tree);
-/*	binary_tree_is_bst(node->left);
-	binary_tree_is_bst(node->right);
-*/	
 	return (l);
 }
